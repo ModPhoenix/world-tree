d97adb2a2313c83d6ed0915d2b563ad723f2f0e1
ModPhoenix <ivanchenko.b@gmail.com>
2022-02-20 12:15:49 +0200

    chore: database for tests

--- migrations/20220218160342_create_user_table.sql
+++ migrations/20220218160342_create_user_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE subscriptions(
+CREATE TABLE users(
   id uuid NOT NULL,
   PRIMARY KEY (id),
   email TEXT NOT NULL UNIQUE,
--- tests/api/auth.rs
+++ tests/api/auth.rs
@@ -1,13 +1,22 @@
+use sqlx::{Connection, PgConnection};
+
 use crate::utils::{spawn_app, GraphQLRequest};
+use graphgram::configuration::get_configuration;
 
 #[tokio::test]
 async fn sign_up_works() {
   // Arrange
   let address = spawn_app();
+  let configuration = get_configuration().expect("Failed to read configuration");
+  let connection_string = configuration.database.connection_string();
+  let mut connection = PgConnection::connect(&connection_string)
+    .await
+    .expect("Failed to connect to Postgres.");
+
   let client = reqwest::Client::new();
   let query = r#"
     mutation {
-      signUp(username: "John", email: "johndoe@example.com", password: "password")
+      signUp(username: "ronin", email: "johndoe@example.com", password: "password")
     }
   "#;
   let request_body = GraphQLRequest::new(query, None);
@@ -26,6 +35,14 @@ async fn sign_up_works() {
     "{\"data\":{\"signUp\":\"sign up\"}}",
     response.text().await.unwrap()
   );
+
+  let saved = sqlx::query!("SELECT email, username FROM users")
+    .fetch_one(&mut connection)
+    .await
+    .expect("Failed to fetch saved subscription.");
+
+  assert_eq!(saved.email, "johndoe@example.com");
+  assert_eq!(saved.username, "ronin");
 }
 
 #[tokio::test]
