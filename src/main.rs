e5f01d867207ff46a8f21da071036dac01128a90
ModPhoenix <ivanchenko.b@gmail.com>
2022-06-05 22:00:32 +0300

    chore: print server url address to console

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use sqlx::PgPool;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
   let configuration = get_configuration().expect("Failed to read configuration.");
+  let application_address = format!("127.0.0.1:{}", configuration.application_port);
   let connection_pool = PgPool::connect(&configuration.database.connection_string())
     .await
     .expect("Failed to connect to Postgres.");
-  let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
+  let listener = TcpListener::bind(application_address.clone())?;
 
+  println!("server started at: http://{}/graphql", application_address);
   run(listener, connection_pool)?.await
 }
